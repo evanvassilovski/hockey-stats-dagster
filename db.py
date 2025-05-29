20ce30019a9eb2263a11a1686c410362345760f8
evanvassilovski <evvassy@gmail.com>
2025-05-29 10:37:09 -0400

    Deployed Database

--- db.py
+++ db.py
@@ -10,10 +10,9 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 def get_engine():
     if not DATABASE_URL:
         raise ValueError("DATABASE_URL environment variable is not set")
     return create_engine(DATABASE_URL)
-# engine = create_engine(DATABASE_URL)
