d68d4f890749eb403cd8e125916c83c4a7a646c7
evanvassilovski <evvassy@gmail.com>
2025-05-30 10:41:21 -0400

    Reconfiguring dotenv

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 
 load_dotenv()
 
+'''
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
@@ -11,6 +12,10 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+'''
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_engine():
     if not DATABASE_URL:
